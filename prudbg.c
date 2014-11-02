2f42bd1e5960c25a040d0c83909e9077a7409776
Rui Paulo <rpaulo@felyko.com>
2014-11-01 23:41:54 -0700

    Implement the help, quit and run commands.

--- prudbg.c
+++ prudbg.c
@@ -25,12 +25,15 @@
  */
 #include <stdio.h>
 #include <unistd.h>
+#include <err.h>
 #include <histedit.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <libpru.h>
+#include <sys/param.h>
 
+static pru_t pru;
 static unsigned int pru_number;
 
 static void __attribute__((noreturn))
@@ -46,18 +49,60 @@ prompt(void)
 {
 	static char p[8];
 
-	snprintf(p, sizeof(p), "pru%d> ", pru_number);
+	snprintf(p, sizeof(p), "(pru%d) ", pru_number);
 
 	return p;
 }
 
+#define	DECL_CMD(name)	\
+	static void cmd_##name(int, const char **)
+
+DECL_CMD(help);
+DECL_CMD(quit);
+DECL_CMD(run);
+
+static struct commands {
+	const char	*cmd;
+	const char	*help;
+	void		(*handler)(int, const char **);
+} cmds[] = {
+	{ "help", "Show a list of all commands.", cmd_help },
+	{ "quit", "Quit the PRU debugger.", cmd_quit },
+	{ "run", "Starts execution on the PRU.", cmd_run },
+};
+
+/*
+ * Implementation of all the debugger commands.
+ */
+static void
+cmd_help(int argc __unused, const char *argv[] __unused)
+{
+	printf("The following is a list of built-in commands:\n\n");
+	for (unsigned int i = 0; i < nitems(cmds); i++)
+		printf("%-10s -- %s\n", cmds[i].cmd, cmds[i].help);
+}
+
+static void __attribute__((noreturn))
+cmd_quit(int argc __unused, const char *argv[] __unused)
+{
+	exit(0);
+}
+
+static void
+cmd_run(int argc __unused, const char *argv[] __unused)
+{
+	pru_enable(pru, pru_number);
+	pru_wait(pru, pru_number);
+}
+
 static int
-main_interface(pru_t pru __unused)
+main_interface(void)
 {
 	EditLine *elp;
 	History *hp;
 	Tokenizer *tp;
 	int count = 0;
+	unsigned int i;
 	const char *line;
 	int t_argc;
 	const char **t_argv;
@@ -82,14 +127,22 @@ main_interface(pru_t pru __unused)
 	el_source(elp, NULL);
 	do {
 		line = el_gets(elp, &count);
-		if (line) {
-			tok_str(tp, line, &t_argc, &t_argv);
-			if (t_argc) {
-				printf("%s\n", t_argv[0]);
+		if (line == NULL)
+			break;
+		tok_str(tp, line, &t_argc, &t_argv);
+		if (t_argc == 0)
+			continue;
+		for (i = 0; i < nitems(cmds); i++) {
+			if (strcmp(t_argv[0], cmds[i].cmd) == 0) {
+				cmds[i].handler(t_argc - 1, t_argv + 1);
+				break;
 			}
 		}
+		if (i == nitems(cmds)) {
+			printf("error: invalid command '%s'\n", t_argv[0]);
+		}
 		tok_reset(tp);
-	} while (line != NULL && count != -1);
+	} while (count != -1);
 	el_end(elp);
 	history_end(hp);
 
@@ -101,7 +154,6 @@ main(int argc, char *argv[])
 {
 	int ch;
 	char *type;
-	pru_t pru;
 	pru_type_t pru_type;
 
 	type = NULL;
@@ -118,26 +170,30 @@ main(int argc, char *argv[])
 	argc -= optind;
 	argv += optind;
 	if (type == NULL) {
-		fprintf(stderr, "%s: missing type (-t)\n", getprogname());
+		warnx("missing type (-t)");
 		usage();
 	}
 	if (argc == 0) {
-		fprintf(stderr, "%s: missing binary file\n", getprogname());
+	        warnx("missing binary file");
 		usage();
 	}
 	pru_type = pru_name_to_type(type);
 	if (pru_type == PRU_TYPE_UNKNOWN) {
-		fprintf(stderr, "%s: invalid type '%s'\n", getprogname(),
-		    type);
+		warnx("invalid type '%s'", type);
 		usage();
 	}
 	pru = pru_alloc(pru_type);
-	if (pru == NULL) {
-		fprintf(stderr, "%s: unable to allocate PRU structure: %s\n",
-		    getprogname(), strerror(errno));
-		return 1;
-	}
+	if (pru == NULL)
+		err(1, "unable to allocate PRU structure");
 	pru_reset(pru, pru_number);
+	printf("Uploading '%s' to PRU %d: ", argv[0], pru_number);
+	fflush(stdout);
+	if (pru_upload(pru, pru_number, argv[0]) != 0) {
+		printf("\n");
+		pru_free(pru);
+		err(1, "could not upload file");
+	}
+	printf("done.\n");
 
-	return main_interface(pru);
+	return main_interface();
 }
